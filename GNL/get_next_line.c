46b06445f7872777a2ba9a22ddb2a31cd85f9b2c
Zied Harzi <zharzi@student.42angouleme.fr>
2022-06-12 01:30:46 +0200

    nothing

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: zharzi <zharzi@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 18:20:29 by zharzi            #+#    #+#             */
-/*   Updated: 2022/05/31 14:01:46 by zharzi           ###   ########.fr       */
+/*   Updated: 2022/06/12 01:25:46 by zharzi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int BUFFER_SIZE = 42;////////////////////////////////////////////A SUPPRIMER
+int	BUFFER_SIZE = 42;////////////////////////////////////////////A SUPPRIMER
 
 char	*get_next_line(int fd)
 {
